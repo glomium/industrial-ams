1ebff0916faf8bc9d9a1623db93d7555ebc6e111
Sebastian Braun <sebastian@elmnt.de>
2019-12-05 11:24:30 +1100

    bugfix in cfssl

--- iams/cfssl.py
+++ iams/cfssl.py
@@ -18,19 +18,24 @@ def get_ca_public_key():
     return None
 
 
-def get_certificate(name, image=None, version=None, algo="rsa", size="2096"):
+def get_certificate(name, hosts=None, image=None, version=None, algo="rsa", size=2096):
 
     if image is None and version is None:
         cn = name
-        profile = "client"
+        if hosts is None:
+            profile = "client"
+            hosts = [""]
+        else:
+            profile = "server"
     else:
         cn = "%s:%s:%s" % (name, image, version)
         profile = "peer"
+        hosts = ["127.0.0.1", "localhost", name]
 
     url = 'http://%s/api/v1/cfssl/newcert' % os.environ.get('IAMS_CFSSL')
     data = {
         "request": {
-            "hosts": [""],
+            "hosts": hosts,
             "CN": cn,
             "key": {
                 "algo": algo,
@@ -39,5 +44,6 @@ def get_certificate(name, image=None, version=None, algo="rsa", size="2096"):
             "profile": profile,
         },
     }
+    logger.debug('request to %s: %s', url, data)
     response = requests.post(url, json=data).json()
     return response

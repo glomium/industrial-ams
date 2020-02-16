8fbc8e7ad20c048029419ef45b571aa6bd4bea2c
Sebastian Braun <sebastian@elmnt.de>
2020-02-16 12:24:25 +0100

    remove unused files

--- certificates.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# create secrets for envoy
-echo "crt" | docker secret create -l iams.certificates.crt=envoy envoy.crt -
-echo "key" | docker secret create -l iams.certificates.key=envoy envoy.key -
